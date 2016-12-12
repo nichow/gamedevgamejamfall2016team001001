032f41cb474eeaa2fe3221e10283779a7a7d716f
Nick Garcia <nhgarcia27@gmail.com>
2016-12-12 12:02:27 -0500

    arc movement for some worm

---
+++ afterdark/Assets/worms/movement_arc.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement_arc : MonoBehaviour {
+
+    public float upThrust;
+    public float rightThrust;
+    public float playerThrust;
+    public Rigidbody2D rb;
+    public GameObject player;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(transform.up * upThrust);
+        rb.AddForce(transform.right * rightThrust);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        rb.AddForce(player.transform.position * playerThrust);
+    }
+}
