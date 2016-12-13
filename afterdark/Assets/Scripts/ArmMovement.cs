19760138177cbe29659e2a7bebd9d509b079c762
Reapeth <dt1305@nyu.edu>
2016-12-13 11:06:05 -0500

    Platform stuff.

---
+++ afterdark/Assets/Scripts/ArmMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArmMovement : MonoBehaviour {
+    public Vector3 mousePos;
+    public Vector3 objectPos;
+    public float angle;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        mousePos = Input.mousePosition;
+        mousePos.z = 5.23f;
+        objectPos = Camera.main.WorldToScreenPoint(transform.position);
+        mousePos.x = mousePos.x - objectPos.x;
+        mousePos.y = mousePos.y - objectPos.y;
+        angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+	}
+}
