07b31eb331f9c8aacf6bf7cb4ec06e0be636d512
Unknown <jams.melnick@gmail.com>
2016-12-14 10:12:18 -0500

    Updated Menus
    Menus should now actually function

---
+++ afterdark/Assets/Scripts/Menu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+	public void GoToMenu(){
+		SceneManager.LoadScene ("Menu");
+	}
+		
+	public void GoToHowTo(){
+		SceneManager.LoadScene ("HowToScene");
+	}
+
+	public void GoToLore(){
+		SceneManager.LoadScene ("LoreScene");
+	}
+
+	public void GoToGame(){
+		SceneManager.LoadScene ("PlatformScene");
+	}
+}
